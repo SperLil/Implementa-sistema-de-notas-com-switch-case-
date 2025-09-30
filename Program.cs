a85fee9d0a7da14634b3001f5d1c7aae89b7da48
Lil Passos <lilsper13@gmail.com>
2025-09-30 00:33:23 -0300

    Adiciona código do projeto e .gitignore

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace GradeClassifier;
+    class Program
+{
+    public static void Main()
+    {
+        /*  Console.WriteLine("====Dia da Semana====");
+          Console.WriteLine("Digite um numero de 1 a 7: ");
+          int dayOfWeek = Convert.ToInt32(Console.ReadLine());
+
+          switch (dayOfWeek)
+          {
+              case 1  : 
+                  Console.WriteLine("O dia da semana é semana é Domingo");
+                  break;
+
+              case 2  :
+                  Console.WriteLine("O dia da semana é Segunda-feira");
+                  break;
+
+              case 3  :
+                  Console.WriteLine("O dia da semana é Terça-feira");
+                  break;
+
+              case 4  :
+                  Console.WriteLine("O dia da semana é Quarta-feira");
+                  break;
+
+              case 5:
+                  Console.WriteLine("O dia da semana é Quinta-feira");
+                  break;
+              case 6:
+                  Console.WriteLine("O dia da semana é  Sexta-feira");
+                  break;
+              case 7:
+                  Console.WriteLine("O dia da semana é  Sabado");
+                  break;
+
+              default:
+                  Console.WriteLine("O dia escolhido é invalido");
+                  break;
+          }*/
+
+        Console.WriteLine("==DEsenpenho Escolar==");
+        Console.WriteLine("Digite sua nota: ");
+        int score = Convert.ToInt32(Console.ReadLine());
+
+        switch (score)
+        {
+            case 10:
+            case 9:
+                Console.WriteLine("ótimo");
+                break;
+            case 8:
+            case 7:
+                Console.WriteLine("Bom");
+                break;
+            case 6:
+                Console.WriteLine("Regular");
+                break;
+            case 5:
+                Console.WriteLine("Estude mais");
+                break;
+                 
+            default:
+                Console.WriteLine("Péssimo");
+                break;
+        }
+    }
+}
